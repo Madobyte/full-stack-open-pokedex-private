6eb3f0ac4aa91ab17911614e99d3a8a390cc0318
Marc Bambalan <marcbambi@gmail.com>
2023-05-29 20:41:21 +0800

    Add health check and rollback

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`)
