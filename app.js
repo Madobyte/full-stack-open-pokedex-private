e1733720ed4e9f8f6b8f949c1f031c17bc26334d
Marc Bambalan <marcbambi@gmail.com>
2023-05-29 21:11:37 +0800

    Add dummy endpoint

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`)
