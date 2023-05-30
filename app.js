9142b23bf95eb7ec0a05941b316a101993010e68
Marc Bambalan <marcbambi@gmail.com>
2023-05-30 19:44:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
